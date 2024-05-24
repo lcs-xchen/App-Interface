e6c9e0c7025116060d05e8fce4345560cff75861
Cindy Chen <xchen@lcs.on.ca>
2024-05-24 15:13:51 -0400

    Initial Commit

---
+++ App Interface/App_InterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_InterfaceApp.swift
+//  App Interface
+//
+//  Created by Cindy Chen on 2024-05-24.
+//
+
+import SwiftUI
+
+@main
+struct App_InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App Interface/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App Interface
+//
+//  Created by Cindy Chen on 2024-05-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
