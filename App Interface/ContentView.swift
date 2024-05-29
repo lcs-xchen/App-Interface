8d47b30a0d209106811a19f2679727efbf131574
Cindy Chen <xchen@lcs.on.ca>
2024-05-29 11:01:55 -0400

    Completed format of Home page with some issues with spacing between blocks

--- App Interface/ContentView.swift
+++ App Interface/ContentView.swift
@@ -9,63 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        
-        VStack{
-            Text("BoardingBite")
-                .padding(.trailing, 222)
-                .bold()
-                .font(.system(size: 24))
-            
-            ZStack {
-                Rectangle()
-                    .foregroundColor(.myGray)
-                    .frame(width: 370, height: 50)
-                .cornerRadius(8)
-                
-                
-                HStack {
-                    Image(systemName: "magnifyingglass")
-                    Text("Search for recipes")
-                }
-                .padding(.trailing, 170)
-            }
-            
-            ZStack {
-                Rectangle()
-                    .frame(width: 370, height: 130)
-                    .cornerRadius(8)
-                .foregroundColor(.myRed)
-                
-                HStack{
-                    VStack{
-                        ZStack {
-                            Rectangle()
-                                .frame(width: 185, height: 60)
-                            .foregroundColor(.myPink)
-                            .padding(.leading, 3)
-                            
-                            Text("Discover")
-                                .bold()
-                                .font(.system(size: 20))
-                            
-                                
-                        }
-                        
-                        Text("Easy breakfast ideas with 3 ingredients")
-                            .foregroundColor(.white)
-                            .padding(.leading,18)
-                            
-                    }
-                    
-                    Rectangle()
-                        .frame(width: 160, height: 100)
-                        .padding(.trailing, 20)
-                    
-                }
-            }
-        
-        }
+        Rectangle()
     }
 }
 
---
+++ App Interface/Home.swift
@@ -0,0 +1,249 @@
+//
+//  Home.swift
+//  App Interface
+//
+//  Created by Cindy Chen on 2024-05-29.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        
+        
+        VStack{
+            Text("BoardingBite")
+                .padding(.trailing, 222)
+                .bold()
+                .font(.system(size: 24))
+            
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.myGray)
+                    .frame(width: 370, height: 50)
+                .cornerRadius(8)
+                
+                
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                    Text("Search for recipes")
+                }
+                .padding(.trailing, 170)
+            }
+            
+            ZStack {
+                Rectangle()
+                    .frame(width: 370, height: 130)
+                    .cornerRadius(8)
+                .foregroundColor(.myRed)
+                
+                HStack{
+                    VStack{
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 185, height: 60)
+                            .foregroundColor(.myPink)
+                            .padding(.leading, 3)
+                            
+                            Text("Discover")
+                                .bold()
+                                .font(.system(size: 20))
+                            
+                                
+                        }
+                        
+                        Text("Easy breakfast ideas with 3 ingredients")
+                            .foregroundColor(.white)
+                            .padding(.leading,18)
+                            
+                    }
+                    
+                   Image("breakfast")
+                        .resizable()
+                        .frame(width: 160, height: 110)
+                        .padding(.trailing, 20)
+                    
+                }
+            }
+            
+            HStack {
+                Text("Try something new")
+                    .bold()
+                    .padding(.trailing, 115)
+                
+                Text("Explore more")
+                    .font(.system(size: 14))
+            }
+            
+            HStack{
+                ZStack {
+                    Rectangle()
+                        .cornerRadius(8)
+                        .frame(width: 180, height: 200)
+                        .padding(.leading,30)
+                    .foregroundColor(.myGray)
+                    
+                    VStack{
+                        Image("beef")
+                            .resizable()
+                            .frame(width: 140, height: 120)
+                            .padding(.leading,25)
+                        
+                        Text("Beef noodle soup")
+                            .bold()
+                            .font(.system(size: 15))
+                            .padding(.leading,11)
+                        
+                        Text("30 minutes")
+                            .font(.system(size: 10))
+                            .padding(.trailing,60)
+                        
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 140, height: 25)
+                                .cornerRadius(6)
+                                .padding(.leading,30)
+                            .foregroundColor(.myRed)
+                            
+                            Text("Add to Favs")
+                                .foregroundColor(.white)
+                                .font(.system(size: 14))
+                                .padding(.leading,27)
+                        }
+                           
+                    }
+                }
+                
+                  
+                
+                ZStack {
+                    Rectangle()
+                        .cornerRadius(8)
+                        .frame(width: 180, height: 200)
+                        .padding(.leading,30)
+                    .foregroundColor(.myGray)
+                    
+                    VStack{
+                        Image("kimchi")
+                            .resizable()
+                            .frame(width: 140, height: 120)
+                            .padding(.leading,25)
+                        
+                        Text("Kimchi Fried Rice")
+                            .bold()
+                            .font(.system(size: 15))
+                            .padding(.leading,11)
+                        
+                        Text("20 minutes")
+                            .font(.system(size: 10))
+                            .padding(.trailing,60)
+                        
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 140, height: 25)
+                                .cornerRadius(6)
+                                .padding(.leading,30)
+                            .foregroundColor(.myRed)
+                            
+                            Text("Add to Favs")
+                                .foregroundColor(.white)
+                                .font(.system(size: 14))
+                                .padding(.leading,27)
+                        }
+                           
+                    }
+                }
+                
+            }
+            
+            HStack{
+                ZStack {
+                    Rectangle()
+                        .cornerRadius(8)
+                        .frame(width: 180, height: 200)
+                        .padding(.leading,30)
+                    .foregroundColor(.myGray)
+                    
+                    VStack{
+                        Image("wrap")
+                            .resizable()
+                            .frame(width: 140, height: 120)
+                            .padding(.leading,25)
+                        
+                        Text("Greek inspired wrap")
+                            .bold()
+                            .font(.system(size: 15))
+                            .padding(.leading,30)
+                        
+                        Text("30 minutes")
+                            .font(.system(size: 10))
+                            .padding(.trailing,60)
+                        
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 140, height: 25)
+                                .cornerRadius(6)
+                                .padding(.leading,30)
+                            .foregroundColor(.myRed)
+                            
+                            Text("Add to Favs")
+                                .foregroundColor(.white)
+                                .font(.system(size: 14))
+                                .padding(.leading,27)
+                        }
+                           
+                    }
+                }
+                  
+                
+                ZStack {
+                    Rectangle()
+                        .cornerRadius(8)
+                        .frame(width: 180, height: 200)
+                        .padding(.leading,30)
+                    .foregroundColor(.myGray)
+                    
+                    VStack{
+                        Image("pasta")
+                            .resizable()
+                            .frame(width: 140, height: 120)
+                            .padding(.leading,25)
+                        
+                        Text("Tomato & basil pasta")
+                            .bold()
+                            .font(.system(size: 15))
+                            .padding(.leading,30)
+                        
+                        Text("25 minutes")
+                            .font(.system(size: 10))
+                            .padding(.trailing,60)
+                        
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 140, height: 25)
+                                .cornerRadius(6)
+                                .padding(.leading,30)
+                            .foregroundColor(.myRed)
+                            
+                            Text("Add to Favs")
+                                .foregroundColor(.white)
+                                .font(.system(size: 14))
+                                .padding(.leading,27)
+                        }
+                           
+                    }
+                }
+            }
+            
+         
+        }
+        
+        
+    }
+}
+
+    
+#Preview {
+    Home()
+}
+
